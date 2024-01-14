f7c787381f77c83dbf7f2a951256af4665820d92
Prasanna Kumar <kumar22maran@gmail.com>
2024-01-14 18:54:31 +0530

    commit

--- channel_iteration.go
+++ channel_iteration.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
+func NinjaStar(channel chan string, numRounds int) {
+	rand.Seed(time.Now().UnixNano())
+
+	for i := 0; i < numRounds; i++ {
+		score := rand.Intn(10)
+		channel <- fmt.Sprint("You scored:", score)
+	}
+}
+
+func main() {
+	channel := make(chan string)
+	numRounds := 3
+	go NinjaStar(channel, numRounds)
+	for i := 0; i < numRounds; i++ {
+		fmt.Println(<-channel)
+	}
+	fmt.Println(<-channel)
 }
